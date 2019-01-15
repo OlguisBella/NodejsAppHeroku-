2618a073f31b0bc5536ec37df8b2ce72f58065d4
Olga <olguisnofis777@hotmail.com>
2019-01-15 10:24:12 -0500

    Add files via upload
    cambios

--- demo5-postgres/knexfile.js
+++ demo5-postgres/knexfile.js
@@ -1,7 +1,8 @@
 module.exports ={
     development: {
         client: 'postgresql',
-        connection: 'postgresql://postgres:postgres@localhost:5432/clase'
+//        connection: 'postgresql://postgres:postgres@localhost:5432/clase'
+        connection: 'postgres://vlvjlsrdnovmqc:28756c3f54c2dac351fa9be966cc80c363ac9003f7a576b56c8367e2e2504bdd@ec2-23-21-244-254.compute-1.amazonaws.com:5432/djj3fcgodjeph'
     },
     
     production:{
